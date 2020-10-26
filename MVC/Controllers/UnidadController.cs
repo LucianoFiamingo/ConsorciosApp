ddbc1677d3759d191e5e20b8e16acfd1814c3b2e
davidcastillo <davidcastillo1514@gmail.com>
2020-10-26 17:16:31 -0300

    Pantallas Unidad

---
+++ MVC/Controllers/UnidadController.cs
@@ -0,0 +1,27 @@
+﻿using Entities.EDMX;
+using Servicios;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class UnidadController : Controller
+    {
+        [HttpGet]
+        public ActionResult CrearUnidad()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CrearUnidad(Unidad a)
+        {
+            UnidadService.CrearUnidad(a);
+            return RedirectToAction("VerUnidades");
+        }
+        [HttpGet]
+        public ActionResult VerUnidades()
+        {
+            var Lista = UnidadService.ObtenerTodos();
+            return View(Lista);
+        }
+    }
+}
---
+++ Services/UnidadService.cs
@@ -0,0 +1,30 @@
+﻿using Entities.EDMX;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Servicios
+{
+    public class UnidadService
+    {
+        private static List<Unidad> Lista
+        {
+            get
+            {
+                if (HttpContext.Current.Session["Lista"] == null)
+                    HttpContext.Current.Session["Lista"] = new List<Unidad>();
+                return (List<Unidad>)HttpContext.Current.Session["Lista"];
+            }
+        }
+
+        public static void CrearUnidad(Unidad a)
+        {
+            Lista.Add(a);
+        }
+
+        public static List<Unidad> ObtenerTodos()
+        {
+            return Lista;
+        }
+
+    }
+}
