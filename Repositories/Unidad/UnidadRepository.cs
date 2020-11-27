715c20f23cbb9002e50b45ce6cc5271849e62bf5
davidcastillo <davidcastillo1514@gmail.com>
2020-11-27 17:49:42 -0300

    Agregando Carpetas

---
+++ Repositories/Unidad/UnidadRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using Entities.EDMX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repositories
+{
+    public class UnidadRepository : BaseRepository<Consorcio>
+    {
+        public UnidadRepository(PW3_TP_20202CEntities contexto) : base(contexto)
+        {
+
+        }
+        
+    }
+}
